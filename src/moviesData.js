7c0d9be4b312688674d1eb79c040ab046bb1d56b
kushal <mohitsinghkpsc@gmail.com>
2023-07-29 17:41:41 +0530

    first commit

---
+++ src/moviesData.js
@@ -0,0 +1,32 @@
+export const movies = [{
+    title: "The Avengers",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.0,
+    poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+
+    {
+    title: "Ironman",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    price: 399,
+    rating: 8.9,
+    poster: "https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+    {
+    title: "Doctor Strange in the Multiverse of Madness",
+    plot: "Plot of the movie is related to super natural powers",
+    price: 199,
+    rating: 8.0,
+    poster:"https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg",
+    stars: 0,
+    fav:false,
+    isInCart:false
+    },
+]
